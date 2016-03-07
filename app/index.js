9cc5998f66c4ad46f6f86912527ab647897ab2e3
Paul Grever <paulgrever@Pauls-MacBook-Pro-2.local>
2016-03-06 21:55:08 -0700

    learned basic props

--- app/index.js
+++ app/index.js
@@ -9,4 +9,41 @@ var Hello = React.createClass({
   }
 });
 
-ReactDOM.render(<Hello />, document.getElementById('app'));
+var HelloUser = React.createClass({
+  render: function () {
+    return (
+      <div>Hello, {this.props.name}</div>
+    )
+  }
+});
+
+var FriendsContainer = React.createClass({
+  render: function () {
+    var name = 'Paul Grever';
+    var friends = ['Tahra Johnson', 'Antonio Elias', 'Greg Smiley']
+    return (
+      <div>
+        <h3>Name: {name}</h3>
+        <ShowList names={friends} />
+      </div>
+    )
+  }
+});
+
+var ShowList = React.createClass({
+  render: function () {
+    var listItems = this.props.names.map(function (friend) {
+      return <li> {friend} </li>
+    });
+    return (
+      <div>
+        <h3> Friends </h3>
+        <ul>
+          {listItems}
+        </ul>
+      </div>
+    )
+  }
+})
+
+ReactDOM.render(<FriendsContainer />, document.getElementById('app'));
